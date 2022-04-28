f5ca8b4086102172600200f476ee6320d32cc074
inker <1013671572@qq.com>
2022-04-29 00:46:08 +0800

    1、集成springCloud gateway 测试 2.调整nacos配置中心，增加数据库，mybatis等配置分开

---
+++ gl-gateway/src/main/java/com/mycc/glshop/gateway/GlGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.mycc.glshop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GlGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlGatewayApplication.class, args);
+    }
+
+}
---
+++ gl-gateway/src/test/java/com/mycc/glshop/gateway/GlGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycc.glshop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
